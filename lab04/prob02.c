41762ef3de596b447f4f0d31a6f643e739c8e923
Ian Malerich <ian.malerich@gmail.com>
2017-09-07 15:55:33 -0500

    almost done with prob02

--- lab04/prob02.c
+++ lab04/prob02.c
@@ -5,10 +5,35 @@
 
 #define SIZE 7
 
+// a little utility that gets the submatrix size of the submatrix
+// starting at coordinates (I,J), where a submatrix is one that is
+// square and only consists of the value 1
+int sub_mat_size(int mat[][SIZE], int I, int J) {
+	for (int i=I; i<SIZE; i++) {
+		for (int j=J; j<SIZE; jrr) {
+			// TODO:
+		}
+	}
+}
+
 void find_max_sub(int mat[][SIZE]) {
 	int max_i = -1, max_k = -1, max_size = 0;
 
+	// consider each starting point
 	for (int i=0; i<SIZE; i++) {
+		for (int j=0; j<SIZE; j++) {
+			// not a submatrix, don't worry about it
+			if (mat[i][j] != 1) { continue; }
+
+			// this will at least be 1
+			int size = sub_mat_size(i, j);
+			if (size > max_size) {
+				// new max sub matrix found, update our values
+				max_i = i;
+				max_j = j;
+				max_size = size;
+			}
+		}
 	}
 
 	if (max_i > 0) {
