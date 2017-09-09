6ccfdc462451fb77124558b187babafd338fac57
Ian Malerich <ian.malerich@gmail.com>
2017-09-09 13:56:19 +0000

    finished lab04

--- lab04/prob02.c
+++ lab04/prob02.c
@@ -5,52 +5,73 @@
 
 #define SIZE 7
 
-// a little utility that gets the submatrix size of the submatrix
-// starting at coordinates (I,J), where a submatrix is one that is
-// square and only consists of the value 1
-int sub_mat_size(int mat[][SIZE], int I, int J) {
-	for (int i=I; i<SIZE; i++) {
-		for (int j=J; j<SIZE; jrr) {
-			// TODO:
-		}
-	}
-}
-
-void find_max_sub(int mat[][SIZE]) {
-	int max_i = -1, max_k = -1, max_size = 0;
-
-	// consider each starting point
-	for (int i=0; i<SIZE; i++) {
-		for (int j=0; j<SIZE; j++) {
-			// not a submatrix, don't worry about it
-			if (mat[i][j] != 1) { continue; }
-
-			// this will at least be 1
-			int size = sub_mat_size(i, j);
-			if (size > max_size) {
-				// new max sub matrix found, update our values
-				max_i = i;
-				max_j = j;
-				max_size = size;
-			}
-		}
-	}
-
-	if (max_i > 0) {
-		printf("The maximum square matrix is at (%d, %d) with size %d.\n", 
-			max_i, max_k, max_size);
-	} else {
-		printf("No maximum square matrix found.\n");
-	}
+int MIN(int i, int j) {
+	if (i < j) { return i; }
+	return j;
 }
 
 void print_sq_mat(int mat[][SIZE]) {
 	for (int i=0; i<SIZE; i++) {
-		for (int k=0; k<SIZE; k++) {
-			printf("%d ", mat[i][k]);
+		for (int j=0; j<SIZE; j++) {
+			printf("%d ", mat[i][j]);
 		}
 
 		printf("\n");
+	}
+}
+
+void find_max_sub(int mat[][SIZE]) {
+	// each coordinate of this matrix represents the largest submatrix
+	// for which that coordinate is the lower right value
+	int subs[SIZE][SIZE] = {0};
+
+	// thus the first row and maintain their initial value of 0 or 1
+	// so copy them over here
+	for (int i=0; i<SIZE; i++) { subs[i][0] = mat[i][0]; }
+	for (int j=0; j<SIZE; j++) { subs[0][j] = mat[0][j]; }
+
+	for (int i=1; i<SIZE; i++) {
+		for (int j=1; j<SIZE; j++) {
+			if (mat[i][j] == 0) {
+				// the matrix value is 0
+				// thus, this value is not a submatrix we should consider
+				subs[i][j] = 0;
+
+			} else { // mat[i][j] == 1
+				// check our three adjacent neighbers 
+				// (that we have already looked at)
+				// this 1 extends the minimum existing submatrix
+				// for example, if we have 0 above us, we cannot use
+				// any of those neighbors in a submatrix (so we are only size 0+1=1)
+				// if we have neigbors {2,2,1}, then this index has a maximum
+				// submatrix of size 2 = min(2,2,1)+1
+				subs[i][j] = MIN(MIN(subs[i-1][j-1], subs[i-1][j]), subs[i][j-1]) + 1;
+			}
+		}
+	}
+
+	// now that we know the sizes of each submatrix
+	// loop through our temp array and finding the largest submatrix 
+	// that we can
+	int max_i = -1, max_j = -1, max_size = 0;
+	for (int i=0; i<SIZE; i++) {
+		for (int j=0; j<SIZE; j++) {
+			if (subs[i][j] > max_size) {
+				max_i = i;
+				max_j = j;
+				max_size = subs[i][j];
+			}
+		}
+	}
+
+	// if we found a submatrix, print out its location
+	if (max_i >= 0) {
+		// offset our maximum indecies as these point to the lower right hand corner
+		// and we would like to know the upper left
+	 	printf("The maximum square matrix is at (%d, %d) with size %d.\n", 
+	 		max_i - (max_size-1), max_j - (max_size-1), max_size);
+	} else {
+		printf("No maximum square matrix found.\n");
 	}
 }
 
@@ -58,12 +79,23 @@ int main(int argc, char ** argv) {
 	// seed the random number generator using the system clock
 	srand(time(NULL));
 
-	// generate a random 7x7 matrix made up of 1's and 0's
-	int mat[SIZE][SIZE];
-	for (int i=0; i<SIZE; i++) {
-		for (int k=0; k<SIZE; k++) {
-			mat[i][k] = rand() % 2;
-	}}
+	// given example matrix of size 7x7
+	int mat[SIZE][SIZE] = {
+		{1, 0, 1, 0, 1, 0, 0},
+		{1, 1, 1, 0, 1, 0, 0},
+		{1, 0, 1, 1, 1, 0, 0},
+		{1, 0, 1, 1, 1, 0, 0},
+		{1, 0, 1, 1, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}
+	};
+
+	// ... or use the folliwng to randomize it
+	
+	// for (int i=0; i<SIZE; i++) {
+	// 	for (int j=0; j<SIZE; j++) {
+	// 		mat[i][j] = rand() % 2;
+	// }}
 
 	printf("Given matrix is: \n\n");
 	print_sq_mat(mat);
